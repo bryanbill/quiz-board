27b77b9096dfbd2b7254556f2bc6932a61e98bad
bryanbill <emobrya@gmail.com>
2022-02-06 17:15:47 +0300

    logic:functions - added the core functions handling business logic

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const questions = [{}];
+
+/**
+ * Checks for the answers given by a user
+ *
+ * @returns void
+ */
+
+function check() {}
+
+/**
+ * Resets the questions and answers in the DOM
+ * @returns void
+ */
+function reset() {
+  window.location.reload();
+}
+
+/**
+ * Creates the questions and answers in the DOM
+ * @param {Array} questions
+ */
+function createQuiz(questions) {}
+
+/**
+ * Call the createQuiz function
+ */
+createQuiz(questions);
