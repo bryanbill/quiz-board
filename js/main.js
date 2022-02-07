02eab4a4e4dd15a7e7ac20e555f7db3ca2e0e9c6
bryanbill <emobrya@gmail.com>
2022-02-07 10:17:26 +0300

    logic:questions - removed duplicate questions

--- js/main.js
+++ js/main.js
@@ -54,54 +54,6 @@ const questions = [
 ]
 
 
-let questions = [
-  {
-    question: "What is ",
-    answers: {
-      a: "New Delhi",
-      b: "Mumbai",
-      c: "Chennai",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question: "What is the capital of Pakistan?",
-    answers: {
-      a: "Islamabad",
-      b: "Karachi",
-      c: "Lahore",
-    },
-    correctAnswer: "b",
-  },
-  {
-    question: "What is the capital of China?",
-    answers: {
-      a: "Beijing",
-      b: "Shanghai",
-      c: "Tianjin",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question: "What is the capital of Russia?",
-    answers: {
-      a: "Moscow",
-      b: "St. Petersburg",
-      c: "Novosibirsk",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question: "What is the capital of France?",
-    answers: {
-      a: "Paris",
-      b: "Lyon",
-      c: "Marseille",
-    },
-    correctAnswer: "a",
-  },
-];
-
 /**
  * Checks for the answers given by a user
  *
